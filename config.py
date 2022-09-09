01a72ee0692c5366982d633e02eee8e3c4d5d21f
snappyboy16 <dreykfame@gmail.com>
2022-09-09 11:16:23 +0300

    Commit #1

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = '5430589547:AAFww12JWOSS9xY-ws7xcR6VxRSkyeyoUvQ'
-ADMIN_ID = 708520196
+TOKEN = ''
+ADMIN_ID = 100000000
 BOT_ID = 'https://t.me/hackscam_bot'
 CHANNEL_ID = '@hackscamcommunity'
